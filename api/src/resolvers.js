672fb1dfca8170c8cc989d111e20d00e39145307
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-02-17 21:23:14 +0630

    schema and resolvers

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,16 @@
 
 module.exports = {
   Query: {
-    
+    pets : (_,{input},{models}) => {
+      return models.Pet.findMany({type : input.type})
+    },
+    hello(){ return 'world'}
   },
-  Mutation: {
+  // Mutation: {
     
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
-  },
-  User: {
-    
+  // },
+  Pet : {
+    name : (pet) => "Mrs " +pet.name,
+    type : (pet) => pet.type
   }
 }
--- api/src/schema.js
+++ api/src/schema.js
@@ -4,8 +4,23 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
-
+    type User  {
+        id : ID!,
+        username : String!
+    },
+    type Pet  {
+        id : ID!,
+        createdAt : String!,
+        name : String!,
+        type : String!
+    },
+    input PetInput {
+        type : String!
+    }
+    type Query  {
+        pets(input : PetInput) : [Pet]!
+        hello : String #test query is the first resolve
+    }
 `;
 
 module.exports = typeDefs
